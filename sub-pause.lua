2d928f44f9d06cfd292e771789bed1ecb59d4d81
Ben Kerman <ben@kermanic.org>
2020-10-30 21:18:11 +0100

    Reduce timer delay by 100 ms
    Prevents pausing after a line disappears in most cases

--- sub-pause.lua
+++ sub-pause.lua
@@ -9,7 +9,7 @@ local function set_up_timer(prop_name, sub_text)
 	if sub_text ~= '' then
 		local sub_end = mp.get_property_number("sub-end")
 		local time = mp.get_property_number("time-pos")
-		mp.add_timeout(sub_end - time, pause)
+		mp.add_timeout(sub_end - time - 0.1, pause)
 	end
 end
 
