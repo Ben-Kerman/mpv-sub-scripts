1c6ae10f7ab0cd41b9e4d992b9b19f862f7798ee
Ben Kerman <ben@kermanic.org>
2020-11-04 00:36:24 +0100

    Rewrite
    The script now observes time-pos when subtitles are active and
    only pauses once playback is within 100ms of the end time of the
    active line

--- sub-pause.lua
+++ sub-pause.lua
@@ -1,24 +1,28 @@
 local active = false
+local pause_at = 0
 
-local function pause()
-	if not active then return end
-	mp.set_property("pause", "yes")
+local function handle_tick(prop_name, time_pos)
+	if time_pos ~= nil and pause_at - time_pos < 0.1 then
+		mp.set_property("pause", "yes")
+		mp.unobserve_property(handle_tick)
+	end
 end
 
-local function set_up_timer(prop_name, sub_text)
+local function handle_sub_text_change(prop_name, sub_text)
+	mp.unobserve_property(handle_tick)
 	if sub_text ~= nil and sub_text ~= '' then
-		local sub_end = mp.get_property_number("sub-end")
-		local time = mp.get_property_number("time-pos")
-		mp.add_timeout(sub_end - time - 0.1, pause)
+		pause_at = mp.get_property_number("sub-end") + mp.get_property_number("sub-delay")
+		mp.observe_property("time-pos", "number", handle_tick)
 	end
 end
 
 mp.add_key_binding("n", "sub-pause-toggle", function()
 	if active then
-		mp.unobserve_property(set_up_timer)
+		mp.unobserve_property(handle_sub_text_change)
+		mp.unobserve_property(handle_tick)
 		mp.osd_message("Subtitle pausing disabled")
 	else
-		mp.observe_property("sub-text", "string", set_up_timer)
+		mp.observe_property("sub-text", "string", handle_sub_text_change)
 		mp.osd_message("Subtitle pausing enabled")
 	end
 	active = not active
