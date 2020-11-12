2c1c5947b2a4bd5adac4d0e365b2a5ddb90f0cdf
Ben Kerman <ben@kermanic.org>
2020-11-12 22:53:29 +0100

    [sub-skip] Change start/end handling

--- sub-skip.lua
+++ sub-skip.lua
@@ -5,7 +5,7 @@ local min_skip_time = 3
 local start_offset = 0
 local end_offset = 1
 local speed_skip_speed = 2.5
-local skip_start, skip_end
+local last_sub_end, next_sub_start
 
 function calc_next_delay()
 	local initial_delay = mp.get_property_number("sub-delay")
@@ -61,19 +61,19 @@ end
 
 local initial_speed = 1
 function handle_tick(prop_name, time_pos)
-	if not sped_up and time_pos > skip_start then
+	if not sped_up and time_pos > last_sub_end + start_offset then
 		if seek_skip then start_seek_skip()
 		else
 			initial_speed = mp.get_property_number("speed")
 			mp.set_property_number("speed", speed_skip_speed)
 			sped_up = true
 		end
-	elseif sped_up and skip_end == nil then
+	elseif sped_up and next_sub_start == nil then
 		local next_delay = calc_next_delay()
 		if next_delay ~= nil then
-			skip_end = time_pos + next_delay - end_offset
+			next_sub_start = time_pos + next_delay
 		end
-	elseif sped_up and time_pos > skip_end then
+	elseif sped_up and time_pos > next_sub_start - end_offset then
 		mp.unobserve_property(handle_tick)
 		mp.set_property_number("speed", initial_speed)
 		sped_up = false
@@ -83,7 +83,7 @@ end
 
 function end_skip()
 	mp.observe_property("sub-text", "string", handle_sub_text_change)
-	skip_start, skip_end = nil
+	last_sub_end, next_sub_start = nil
 end
 
 function handle_sub_text_change(prop_name, sub_text)
@@ -92,14 +92,14 @@ function handle_sub_text_change(prop_name, sub_text)
 		local time_pos = mp.get_property_number("time-pos")
 		local function start_skip()
 			mp.unobserve_property(handle_sub_text_change)
-			skip_start = time_pos + start_offset
+			last_sub_end = time_pos
 			mp.observe_property("time-pos", "number", handle_tick)
 		end
 
 		if next_delay == nil then start_skip()
 		elseif next_delay < min_skip_time then return
 		else
-			skip_end = time_pos + next_delay - end_offset
+			next_sub_start = time_pos + next_delay
 			start_skip()
 		end
 	end
@@ -107,9 +107,10 @@ end
 
 mp.add_key_binding("Ctrl+n", "sub-skip-toggle", function()
 	if active then
+		seek_skip_timer:kill()
 		mp.unobserve_property(handle_sub_text_change)
 		mp.unobserve_property(handle_tick)
-		skip_start, skip_end = nil
+		last_sub_end, next_sub_start = nil
 		sped_up = false
 		mp.osd_message("Non-subtitle skip disabled")
 	else
